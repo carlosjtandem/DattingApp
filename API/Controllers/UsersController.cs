0464c51d30f32728c0f246be917b1422551505c3
carlosjtandem <soycarlosjavier14@gmail.com>
2022-07-12 19:13:57 -0500

    Added angular prj and async calls in API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync() ;
+            return users;
+        }
+
+        //api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
