eda9d0ecbf93040b4c49dfdbb2ba52ce37fd9b9d
carlosjtandem <soycarlosjavier14@gmail.com>
2022-07-20 17:16:08 -0500

    Added https and enabled CORS

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<DataContext>(
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("corsapp");
 
 app.UseHttpsRedirection();
 
